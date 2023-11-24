9c66c4813a5fd5e742cc07a832dfa31d3f77c622
Jaya Surya G <jayasuryagorle@gmail.com>
2023-11-24 12:13:57 +0530

    add server

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import Developers from './components/Developers';
+
+
+function App() {
+  return (
+    <div className="App">
+    
+  <Developers/>
+
+    </div>
+  );
+}
+
+export default App;
