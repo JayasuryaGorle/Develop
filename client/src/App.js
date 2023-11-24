630e76f5b31ec92e39d8544b00a58dd84712cff5
Jaya Surya G <jayasuryagorle@gmail.com>
2023-11-24 12:41:24 +0530

    add changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,30 @@
 // import logo from './logo.svg';
 import './App.css';
-import Developers from './components/Developers';
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import About from './components/About';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import Home from './components/Home';
+import Task from './components/Task';
+
+
 
 
 function App() {
   return (
     <div className="App">
     
-  <Developers/>
+    <BrowserRouter>
+     <Routes>
+     <Route path='/' element={<About/>}></Route>
+     <Route path='/Login' element={<Login/>}></Route>
+     <Route path='/Signup' element={<SignUp/>}></Route>
+     <Route path='/Home' element={<Home/>}></Route>
+     <Route path='/Task' element={<Task/>}></Route>
+      
+     </Routes>
+     </BrowserRouter>
+  
 
     </div>
   );
